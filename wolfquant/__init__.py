413e5fda011b8055a27a6f019c4c0cff1d29b2d7
qisc <qisc@qisc.local>
2017-10-29 10:07:52 +0800

    调整结构

--- wolfquant/__init__.py
+++ wolfquant/__init__.py
@@ -5,5 +5,4 @@ def config(filepath):
     """配置文件
     """
     with open(filepath, encoding='utf8') as json_file:
-        data = json.load(json_file)
-        return data
+        return json.load(json_file)
